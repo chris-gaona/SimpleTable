46c4222a985eb1be917e0a9b1074f9f12c1b8f61
Chris Gaona <thebeloved88@sbcglobal.net>
2021-04-18 18:25:24 -0700

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Chris Gaona on 4/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
